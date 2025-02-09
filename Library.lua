406d6c2537a84df19cfba8811e01baab4b7fe9e3
pajiro <bletkarl428@gmail.com>
2025-02-10 02:14:34 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3519,7 +3519,7 @@ function Library:CreateWindow(...)
 
         if Toggled then
             -- A bit scuffed, but if we're going from not toggled -> toggled we want to show the frame immediately so that the fade is visible.
-            Outer.Visible = true;
+            Outer.Visible = false;
 
             task.spawn(function()
                 -- TODO: add cursor fade?
